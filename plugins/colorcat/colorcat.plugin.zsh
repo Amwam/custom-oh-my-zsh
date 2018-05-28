e634fddf996eb6ce7910d24d51102be3c8331a52
Amit Shah <amit@amwam.me>
2018-05-28 20:34:10 +0100

    Added some plugins

---
+++ plugins/colorcat/colorcat.plugin.zsh
@@ -0,0 +1,9 @@
+# Add syntax highlighting to cat
+# Requires Pygments to be installed (via pip)
+
+cat() {
+    local out colored
+    out=$(/bin/cat $@)
+    colored=$(echo $out | pygmentize -f console -g 2>/dev/null)
+    [[ -n $colored ]] && echo "$colored" || echo "$out"
+}
