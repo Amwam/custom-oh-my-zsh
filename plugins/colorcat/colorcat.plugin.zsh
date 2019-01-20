137238b79cbe1fcd0b75b07674b3b4687bdacea2
Amit Shah <amit@amwam.me>
2019-01-20 18:03:49 +0000

    Remove colorcat

--- plugins/colorcat/colorcat.plugin.zsh
+++
@@ -1,9 +0,0 @@
-# Add syntax highlighting to cat
-# Requires Pygments to be installed (via pip)
-
-cat() {
-    local out colored
-    out=$(/bin/cat $@)
-    colored=$(echo $out | pygmentize -f console -g 2>/dev/null)
-    [[ -n $colored ]] && echo "$colored" || echo "$out"
-}
